72adf751390451fa3e70ce3e2d2e38eb1799c9aa
Julien Ganichot <julien.ganichot@gmail.com>
2015-01-04 05:26:13 +0100

    Mainly UI changes

--- SQL/jweb.sql
+++ SQL/jweb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 04, 2015 at 01:01 AM
+-- Generation Time: Jan 04, 2015 at 05:24 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -30,17 +30,18 @@ CREATE TABLE IF NOT EXISTS `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Caca' AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Caca' AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`id`, `title`, `content`, `timestamp`) VALUES
-(1, 'Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non lorem in lorem lobortis vehicula vel mollis eros. Aenean pharetra id arcu interdum ultricies. In fringilla purus congue lectus eleifend ornare. Sed suscipit mi nec efficitur tincidunt. Maecenas dignissim, enim at hendrerit porttitor, ligula orci vulputate ipsum, ac ornare augue ligula elementum felis. Nullam ut nulla sit amet lectus hendrerit rhoncus.', '2015-01-03 18:48:56');
+INSERT INTO `news` (`id`, `title`, `content`) VALUES
+(1, 'Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non lorem in lorem lobortis vehicula vel mollis eros. Aenean pharetra id arcu interdum ultricies. In fringilla purus congue lectus eleifend ornare. Sed suscipit mi nec efficitur tincidunt. Maecenas dignissim, enim at hendrerit porttitor, ligula orci vulputate ipsum, ac ornare augue ligula elementum felis. Nullam ut nulla sit amet lectus hendrerit rhoncus.'),
+(2, 'News writer test!', 'This is a test! Would be grateful if it works..'),
+(3, 'I just tested', 'And it works fine :D');
 
 -- --------------------------------------------------------
 
@@ -73,13 +74,21 @@ INSERT INTO `product` (`id`, `name`, `description`, `price`) VALUES
 
 CREATE TABLE IF NOT EXISTS `review` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `review` text NOT NULL,
   `rating` int(11) NOT NULL,
   `productId` int(11) NOT NULL,
   `posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='JULIEN SUCKS' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='JULIEN SUCKS' AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `review`
+--
+
+INSERT INTO `review` (`id`, `username`, `review`, `rating`, `productId`, `posted`) VALUES
+(1, 'Ganitzsh', 'Test', 1, 1, '2015-01-04 02:06:56'),
+(2, 'Ganitzsh', 'C la vie tavu', 5, 1, '2015-01-04 03:59:45');
 
 -- --------------------------------------------------------
 
@@ -103,8 +112,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`id`, `username`, `email`, `passwordHash`, `newsletter`, `admin`) VALUES
 (3, 'MARACEULTIME', 'gsagas', '5eae5f7b3f495deff9b77521513fb18a88ed96a8', 1, 0),
-(4, 'Ganitzsh', 'lol', '356a192b7913b04c54574d18c28d46e6395428ab', 0, 0),
-(6, 'Ganitzsh', 'lol', '356a192b7913b04c54574d18c28d46e6395428ab', 0, 0);
+(4, 'Ganitzsh', 'lol', '356a192b7913b04c54574d18c28d46e6395428ab', 0, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
